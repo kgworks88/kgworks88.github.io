c1338d9d795b48d487ce5a369f570d30053e71c5
kgworks88 <kgworksnz@gmail.com>
2023-01-26 23:40:35 +1300

    contact form update

--- ca_contacts/index.php
+++ ca_contacts/index.php
@@ -129,7 +129,7 @@
                             ))}
                             </table>
                         </div>
-                        <ContactForm />
+                        
                     </React.Fragment>
                 );
             }
