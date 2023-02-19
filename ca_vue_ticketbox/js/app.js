93831e4c67eb5656aa8a288a4c8e70064e93142f
kgworks88 <kgworksnz@gmail.com>
2023-02-19 14:01:24 +1300

    ticket box project updates - computed and watch properties

--- ca_vue_ticketbox/js/app.js
+++ ca_vue_ticketbox/js/app.js
@@ -11,11 +11,26 @@ const app = new Vue({
     purchaseAgreementSigned: false
   },
   computed: {
-    fullName: function() {
-      if (this.firstName && this.lastName) {
-        return this.firstName + ' ' + this.lastName;
-      } else {
-        return this.firstName || this.lastName;
+    fullName: {
+      get: function() {
+        if (this.firstName && this.lastName) {
+          return this.firstName + ' ' + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      set: function(newFullName) {
+        const names = newFullName.split(' ');
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+        
+        if (names.length <= 1) {
+          this.firstName = names[0] || '';
+          this.lastName = '';
+        }
       }
     },
     ticketDescription: function() {
@@ -23,13 +38,21 @@ const app = new Vue({
       if (this.ticketType === 'vip') {
         readableTicketType = 'VIP';
       }
-
+     
       let ticketPluralization = 'tickets';
       if (this.ticketQuantity === 1) {
         ticketPluralization = 'ticket';
       }
-
+     
       return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
+    }
+  },
+  watch:{
+    specialRequests: function(newRequests, oldRequests){
+      if (newRequests.toLowerCase().includes('meet and greet') || 
+        newRequests.toLowerCase().includes('meet-and-greet')) {
+        this.ticketType = 'vip';
+      }
     }
   }
 });
